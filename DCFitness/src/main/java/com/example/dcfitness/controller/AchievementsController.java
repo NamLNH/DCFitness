5e7cf8901be7091c25cad58d61959ce3314b09d4
yashmaniar <yashmaniar3@gmail.com>
2024-03-04 16:05:11 -0800

    Update AchievementsController.java

--- DCFitness/src/main/java/com/example/dcfitness/controller/AchievementsController.java
+++ DCFitness/src/main/java/com/example/dcfitness/controller/AchievementsController.java
@@ -2,16 +2,20 @@ package com.example.dcfitness.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.dcfitness.model.AchievementRepository;
 import com.example.dcfitness.model.Achievements;
+import com.example.dcfitness.model.User;
 @RestController
 public class AchievementsController {
 	@Autowired
@@ -34,5 +38,20 @@ public class AchievementsController {
 		}
 	
 	}
+	
+//	@GetMapping("/achievements/{id}")
+//	public ResponseEntity<User> getAchievementById(@PathVariable("id") long id){
+//		List<Achievements> achievement = achRepo.findById(id);
+//		if(achievement.size() > 0) {
+//			return new ResponseEntity<>(achievement.get(0),HttpStatus.OK);
+//		}
+//		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+//	}
+	
+	@GetMapping("/achievements/{id}")
+    public Achievements getAchievementById(@PathVariable Long id) {
+		return achRepo.findById(id).orElse(null);
+    }
+    
 	
 }
