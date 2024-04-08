f7c7332a4bb74c278a59414559c232e87934caac
ASUS <ASUS@HOANGNAM>
2024-04-08 15:53:00 -0700

    Nam done with browse all videos in Home and choose each video to play in VideoView

--- vue_project/src/components/VideoView.vue
+++ vue_project/src/components/VideoView.vue
@@ -2,26 +2,44 @@
     <div>
       <h1>Video View Component</h1>
       <div>
+        <h2>youtube video ID: {{ this.youtubeLink }}</h2>
         <VideoPlayer :youtubeLink="youtubeLink" />
+      </div>
+      <div>        
         <CommentsView />
       </div>
     </div>
   </template>
   
   <script>
+  import VideoService from '@/services/VideoService';
   import CommentsView from "./CommentsView.vue";
   import VideoPlayer from "./VideoPlayer.vue";
+  
   
   export default {
     data() {
       return {
-        youtubeLink: "https://www.youtube.com/watch?v=6JYIGclVQdw", // Provided YouTube link
+        youtubeLink: null, // Provided YouTube link
       };
     },
     components: {
       CommentsView,
       VideoPlayer,
     },
+    mounted(){
+      this.getVideoUrl();
+    },
+    methods:{
+      getVideoUrl(){
+        const videoID = localStorage.getItem("videoID") ?? 1;
+        VideoService.getVideoById(videoID).then(response =>{
+          this.youtubeLink = response.data.videoId;
+        }).catch(error =>{
+          console.log(error);
+        })
+      }
+    }
   };
   </script>
   
