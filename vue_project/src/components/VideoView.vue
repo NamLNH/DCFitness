bc6620437b719f057d091299d024a1e6ebff5d48
yashmaniar <yashmaniar3@gmail.com>
2024-04-08 01:55:08 -0700

    Added video element to CommentsView

--- vue_project/src/components/VideoView.vue
+++ vue_project/src/components/VideoView.vue
@@ -1,24 +1,29 @@
 <template>
     <div>
-        <h1>Video View Component for Nam to work on</h1>
-        <div>
-            <CommentsView />
-        </div>
+      <h1>Video View Component</h1>
+      <div>
+        <VideoPlayer :youtubeLink="youtubeLink" />
+        <CommentsView />
+      </div>
     </div>
-</template>
-
-<script>
-
-import CommentsView from "./CommentsView.vue";
-
-export default {
-    data() {},
-
+  </template>
+  
+  <script>
+  import CommentsView from "./CommentsView.vue";
+  import VideoPlayer from "./VideoPlayer.vue";
+  
+  export default {
+    data() {
+      return {
+        youtubeLink: "https://www.youtube.com/watch?v=6JYIGclVQdw", // Provided YouTube link
+      };
+    },
     components: {
-        CommentsView
-    }
-}
-
-</script>
-
-<style></style>
+      CommentsView,
+      VideoPlayer,
+    },
+  };
+  </script>
+  
+  <style></style>
+  
