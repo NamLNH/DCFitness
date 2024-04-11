cad7e0c6bddb0ec10f0d0b0942c576cca94e83f1
ASUS <ASUS@HOANGNAM>
2024-04-10 21:21:15 -0700

    Full CRUD for video

--- vue_project/src/components/ModifyVideo.vue
+++ vue_project/src/components/ModifyVideo.vue
@@ -1,11 +1,12 @@
 <template>
   <!--Header for web-->
-  <WebHeader/>
+  <WebHeader />
 
   <!--end of header-->
   <div class="add-title" style="margin-left: 20px; margin-right: 20px">
-    <h2>Modify video</h2>    
+    <h2>Modify video</h2>
   </div>
+
   <div class="card-body" style="margin-left: 20px; margin-right: 20px">
     <form method="" action="">
       <div class="row">
@@ -17,8 +18,7 @@
             >
             <div class="col-lg-10 col-md-6 col-sm-12 title-input">
               <input
-                name="name"
-                :value="this.videoTitle"
+                v-model="this.videoTitle"
                 type="text"
                 class="form-control"
                 required
@@ -34,7 +34,7 @@
             <div class="col-lg-10 col-md-6 col-sm-12">
               <input
                 name="isbn"
-                :value="this.videoUrl"
+                v-model="this.videoUrl"
                 type="text"
                 class="form-control"
                 required
@@ -51,10 +51,19 @@
               >Category:
             </label>
             <div class="col-lg-10 col-md-6 col-sm-12">
-              <select name="category" class="form-control" required>
-                <option value="1">Option 1</option>
-                <option value="2">Option 2</option>
-                <option value="3">Option 3</option>
+              <select
+                v-model="this.videoCategory"
+                class="form-control"
+                required
+              >
+                <option
+                  v-for="(category, index) in categories"
+                  :key="index"
+                  v-bind:value="{ id: category.id, name: category.name }"
+                  :selected="this.videoCategory.name === category.name"
+                >
+                  {{ category.name }}
+                </option>
               </select>
             </div>
           </div>
@@ -67,7 +76,7 @@
             <div class="col-lg-10 col-md-6 col-sm-12">
               <input
                 name="quantity"
-                :value="this.videoAuthor"
+                v-model="this.videoAuthor"
                 type="text"
                 class="form-control"
                 required
@@ -78,56 +87,88 @@
       </div>
       <div class="submit-div">
         <div class="btn update-div">
-          <button
-            type="submit"
-            class="btn btn-primary btn-update"
-            name="action"
-            value="update"
-          >
+          <button class="btn btn-primary btn-update" @click="updateVideo">
             Update Book
           </button>
         </div>
-        
       </div>
     </form>
   </div>
-  
 </template>
 
 <script>
-import WebHeader from "@/components/WebHeader.vue"
-import VideoService from '@/services/VideoService';
+import WebHeader from "@/components/WebHeader.vue";
+import VideoService from "@/services/VideoService";
+import CategoryService from "@/services/CategoryService";
 
 export default {
   data() {
-    return{
-        video: null,
-        videoTitle: null,
-        videoUrl: null,
-        videoAuthor: null,
-        videoCategory: null
-    }  
-
+    return {
+      video: null,
+      videoTitle: null,
+      videoUrl: null,
+      videoAuthor: null,
+      videoCategory: null,
+      categories: [],
+    };
   },
-  components :{
-    WebHeader
+  components: {
+    WebHeader,
   },
   methods: {
-    getVideoById(){
-        const videoID = localStorage.getItem("videoID") ?? 1;
-        VideoService.getVideoById(videoID).then(response =>{
+    getVideoById() {
+      const videoID = localStorage.getItem("videoID") ?? 1;
+      VideoService.getVideoById(videoID)
+        .then((response) => {
           this.videoTitle = response.data.title;
           this.videoUrl = response.data.url;
           this.videoAuthor = response.data.author;
-
-        }).catch(error =>{
-          console.log(error);
+          this.videoCategory = response.data.category;
         })
-    }
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    getCategories() {
+      CategoryService.getCategories()
+        .then((response) => {
+          this.categories = response.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    updateVideo() {
+      const id = localStorage.getItem("videoID") ?? 1;
+      const videoThumbnail =
+        "https://i.ytimg.com/vi/" +
+        this.videoUrl.substring(17, 28) +
+        "/default.jpg";
+      const videoId = this.videoUrl.substring(17, 28);
+
+      const body = {
+        title: this.videoTitle,
+        url: this.videoUrl,
+        author: this.videoAuthor,
+        thumbnail: videoThumbnail,
+        videoId: videoId,
+        category: this.videoCategory,
+      };
+
+      VideoService.updateVideoByID(id, body)
+        .then((response) => {
+          console.log(response.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+      this.$router.push({ name: "homePage" });
+    },
   },
-  mounted(){
+  mounted() {
     this.getVideoById();
-  }
+    this.getCategories();
+  },
 };
 </script>
 
